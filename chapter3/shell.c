82af444ab5a961761c75a8d80cd0c65087a0da95
Giovanni Pascotto Bonin <pbonin.giovanni@gmail.com>
2016-03-27 22:41:09 -0400

    add functionality to shell.c

--- chapter3/shell.c
+++ chapter3/shell.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 
 #define MAX_LINE 80 /* The maximum length command */
@@ -10,6 +11,7 @@ int main(void)
     char *args[MAX_LINE/2 + 1];
     char input[MAX_LINE];
     int should_run = 1;
+    pid_t pid;
 
     while (should_run)
     {
@@ -20,16 +22,42 @@ int main(void)
         char *newArg;
         int i = 0;
         newArg = strtok(input, " ");
-        while (newArg != NULL)
+        while (1)
         {
             args[i] = newArg;
             i++;
             newArg = strtok(NULL, " ");
+            if (newArg == NULL)
+            {
+                int strLen = strlen(args[i - 1]);
+                args[i - 1][strLen - 1] = 0;
+                args[i] = NULL;
+                break;
+            }
         }
-        printf("%s", args[0]);
-        if (strcmp(args[0], "exit\n") == 0)
+        if (strcmp(args[0], "exit") == 0)
         {
             should_run = 0;
+            return 0;
+        }
+        pid = fork();
+        if (pid < 0)
+        {
+            fprintf(stderr, "Error creating child process");
+        }
+        else if (pid == 0)
+        {
+            execvp(args[0], args);
+            _exit(0);
+        }
+        else
+        {
+            /*
+            if (strcmp(args[i - 1], "&\n") == 0)
+            {
+                printf("works");
+                waitpid(pid, NULL, 0);
+            }*/
         }
     }
     return 0;
